7d55c2151881e6b53dfb8fc1377af58d38525861
BakirFirstREPO <108557728+bmurad85@users.noreply.github.com>
2022-07-25 23:06:59 -0500

    I made this change

--- src/src/SPRINT1/ticket111.java
+++ src/src/SPRINT1/ticket111.java
@@ -1,19 +1,2 @@
-package SPRINT1;
-
-import java.util.Scanner;
-
-public class ticket111 {
-    public static void main(String[] args) {
-
-
-        Scanner input = new Scanner(System.in);
-        System.out.println("please enter the numbers");
-
-        int number1=input.nextInt();
-        int number2=input.nextInt();
-
-
-        System.out.println("max number is "+Math.max(number1,number2));
-
-    }
+package SPRINT1;public class Ticket111 {
 }
